7f582120d4ff2fe26cf160ea8266143048a874ac
Karlangas91 <cbadillarjl@gmail.com>
2025-02-09 20:06:44 -0600

    Añadido script para agregar role_id en users

---
+++ scripts/addRoleColumn.js
@@ -0,0 +1,14 @@
+const pool = require('../config/database');
+
+async function addRoleColumn() {
+    try {
+        await pool.query(`
+            ALTER TABLE users ADD COLUMN IF NOT EXISTS role_id INTEGER REFERENCES roles(id) DEFAULT 1;
+        `);
+        console.log("✅ Columna 'role_id' añadida con éxito.");
+    } catch (error) {
+        console.error("❌ Error al añadir la columna 'role_id':", error);
+    }
+}
+
+addRoleColumn();
