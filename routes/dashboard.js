0e24dfed1a40e6efd0718123495d700cc2f6b78c
Karlangas91 <cbadillarjl@gmail.com>
2025-02-12 15:09:08 -0600

    Incluir sidebar correctamente en el layout y estructurar vista de dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -18,7 +18,6 @@ router.get('/dashboard', isAuthenticated, (req, res) => {
         successMessage: req.flash('successMessage'),
         errorMessage: req.flash('errorMessage'),
         layout: 'layout',  // Aquí usamos layout.ejs
-        body: '<h1>Bienvenido al Dashboard</h1>'
     });
 });
 
